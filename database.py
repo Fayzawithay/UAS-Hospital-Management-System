950b2078662919dd07577be66c9baa2d7d045e7d
santarikardo <santarikardo2@gmail.com>
2025-12-09 19:09:01 +0700

    Update database.py

--- database.py
+++ database.py
@@ -12,9 +12,9 @@ DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 
-DATABASE_URL = "mysql+pymysql://root:Zxcvbn123432@localhost:3306/myhospital_new"
+DATABASE_URL = "mysql+pymysql://root:@localhost:3306/myhospital"
 # kalau MySQL pakai password:
-# DATABASE_URL = "mysql+pymysql://root:PASSWORD@localhost:3306/myhospital_new"
+# DATABASE_URL = "mysql+pymysql://root:PASSWORD@localhost:3306/myhospital"
 
 engine = create_engine(DATABASE_URL, echo=True, future=True)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
